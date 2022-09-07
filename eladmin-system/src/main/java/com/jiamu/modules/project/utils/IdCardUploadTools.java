bd96c13a443a388aa66427f330c1e350228c5243
fengwen <1359086686@qq.com>
2022-09-07 08:44:33 +0800

    工具身份证上传测试类

---
+++ eladmin-system/src/main/java/com/jiamu/modules/project/utils/IdCardUploadTools.java
@@ -0,0 +1,11 @@
+package com.jiamu.modules.project.utils;
+/**
+ * @ProjectName:    vue_springboot-backend 
+ * @Package:        com.jiamu.modules.project.utils
+ * @ClassName:      IdCardUploadTools
+ * @Author:     fengwen
+ * @Description:  身份证上传工具类  
+ * @Date:    2022/9/6 10:42
+ * @Version:    1.0.0
+ */public class IdCardUploadTools {
+}
